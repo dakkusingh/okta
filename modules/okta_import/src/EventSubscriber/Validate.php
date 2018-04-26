acbdcf2ce8cff77f88607570095e8fa4b9ec4606
dakku <dakku@97634.no-reply.drupal.org>
2018-04-26 17:09:40 +0100

    Validate Event Subscriber.

---
+++ modules/okta_import/src/EventSubscriber/Validate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\okta_import\EventSubscriber;
+
+use Drupal\okta_import\Event\ValidateEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class Validate implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[ValidateEvent::OKTA_IMPORT_VALIDATE] = 'validate';
+    return $events;
+  }
+
+  /**
+   * Alter user before validate.
+   *
+   * @param \Drupal\okta_import\Event\ValidateEvent $event
+   * Validate Event.
+   */
+  public function validate(ValidateEvent $event) {
+    $emails = $event->getEmails();
+    ksm($emails);
+  }
+
+}
